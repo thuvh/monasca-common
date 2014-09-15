2dd6c5299940ff0e3c86ebadc0a1fa17273387dd
Jonathan Halterman <jhalterman@gmail.com>
2014-09-15 11:52:17 -0700

    WIP - Event changes for alarmed-metrics support
    Change-Id: I86c394d33d93cc14af8900a8dd240e0058eb31b2

--- java/monasca-common-model/src/main/java/com/hpcloud/mon/common/event/AlarmDefinitionDeletedEvent.java
+++ java/monasca-common-model/src/main/java/com/hpcloud/mon/common/event/AlarmDefinitionDeletedEvent.java
@@ -14,32 +14,29 @@
 package com.hpcloud.mon.common.event;
 
 import java.io.Serializable;
+import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonRootName;
-import com.hpcloud.mon.common.model.alarm.AlarmState;
+import com.hpcloud.mon.common.model.metric.MetricDefinition;
 
 /**
- * Represents an alarm having been updated.
+ * Represents an alarm definition having been deleted.
  */
-@JsonRootName(value = "alarm-updated")
-public class AlarmUpdatedEvent implements Serializable {
-  private static final long serialVersionUID = -890221414491823712L;
+@JsonRootName(value = "alarm-definition-deleted")
+public class AlarmDefinitionDeletedEvent implements Serializable {
+  private static final long serialVersionUID = -845914476456541787L;
 
   public String tenantId;
-  public String alarmId;
   public String alarmDefinitionId;
-  public AlarmState alarmState;
-  public AlarmState oldAlarmState;
+  public Map<String, MetricDefinition> subAlarmMetricDefinitions;
 
-  public AlarmUpdatedEvent() {}
+  public AlarmDefinitionDeletedEvent() {}
 
-  public AlarmUpdatedEvent(String tenantId, String alarmId, String alarmDefinitionId,
-      AlarmState alarmState, AlarmState oldAlarmState) {
+  public AlarmDefinitionDeletedEvent(String tenantId, String alarmDefinition,
+      Map<String, MetricDefinition> subAlarmMetricDefinitions) {
     this.tenantId = tenantId;
-    this.alarmId = alarmId;
-    this.alarmDefinitionId = alarmDefinitionId;
-    this.alarmState = alarmState;
-    this.oldAlarmState = oldAlarmState;
+    this.alarmDefinitionId = alarmDefinition;
+    this.subAlarmMetricDefinitions = subAlarmMetricDefinitions;
   }
 
   @Override
@@ -48,22 +45,18 @@ public class AlarmUpdatedEvent implements Serializable {
       return true;
     if (obj == null)
       return false;
-    if (!(obj instanceof AlarmUpdatedEvent))
+    if (!(obj instanceof AlarmDefinitionDeletedEvent))
       return false;
-    AlarmUpdatedEvent other = (AlarmUpdatedEvent) obj;
+    AlarmDefinitionDeletedEvent other = (AlarmDefinitionDeletedEvent) obj;
     if (alarmDefinitionId == null) {
       if (other.alarmDefinitionId != null)
         return false;
     } else if (!alarmDefinitionId.equals(other.alarmDefinitionId))
       return false;
-    if (alarmId == null) {
-      if (other.alarmId != null)
+    if (subAlarmMetricDefinitions == null) {
+      if (other.subAlarmMetricDefinitions != null)
         return false;
-    } else if (!alarmId.equals(other.alarmId))
-      return false;
-    if (alarmState != other.alarmState)
-      return false;
-    if (oldAlarmState != other.oldAlarmState)
+    } else if (!subAlarmMetricDefinitions.equals(other.subAlarmMetricDefinitions))
       return false;
     if (tenantId == null) {
       if (other.tenantId != null)
@@ -78,17 +71,17 @@ public class AlarmUpdatedEvent implements Serializable {
     final int prime = 31;
     int result = 1;
     result = prime * result + ((alarmDefinitionId == null) ? 0 : alarmDefinitionId.hashCode());
-    result = prime * result + ((alarmId == null) ? 0 : alarmId.hashCode());
-    result = prime * result + ((alarmState == null) ? 0 : alarmState.hashCode());
-    result = prime * result + ((oldAlarmState == null) ? 0 : oldAlarmState.hashCode());
+    result =
+        prime * result
+            + ((subAlarmMetricDefinitions == null) ? 0 : subAlarmMetricDefinitions.hashCode());
     result = prime * result + ((tenantId == null) ? 0 : tenantId.hashCode());
     return result;
   }
 
   @Override
   public String toString() {
-    return "AlarmUpdatedEvent [tenantId=" + tenantId + ", alarmId=" + alarmId
-        + ", alarmDefinitionId=" + alarmDefinitionId + ", alarmState=" + alarmState
-        + ", oldAlarmState=" + oldAlarmState + "]";
+    return String.format(
+        "AlarmDefinitionDeletedEvent [tenantId=%s, alarmDefinitionId=%s, subAlarmIds=%s]",
+        tenantId, alarmDefinitionId, subAlarmMetricDefinitions);
   }
 }
--- java/monasca-common-model/src/main/java/com/hpcloud/mon/common/event/AlarmDeletedEvent.java
+++ java/monasca-common-model/src/main/java/com/hpcloud/mon/common/event/AlarmDeletedEvent.java
@@ -1,46 +1,39 @@
 /*
  * Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- * implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.hpcloud.mon.common.event;
 
 import java.io.Serializable;
-import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonRootName;
-import com.hpcloud.mon.common.model.metric.MetricDefinition;
 
 /**
  * Represents an alarm having been deleted.
  */
 @JsonRootName(value = "alarm-deleted")
 public class AlarmDeletedEvent implements Serializable {
-  private static final long serialVersionUID = -845914476456541787L;
+  private static final long serialVersionUID = -988406683520841426L;
 
   public String tenantId;
   public String alarmId;
-  public Map<String, MetricDefinition> subAlarmMetricDefinitions;
+  public String alarmDefinitionId;
 
-  public AlarmDeletedEvent() {
-  }
+  public AlarmDeletedEvent() {}
 
-  public AlarmDeletedEvent(String tenantId, String alarmId,
-      Map<String, MetricDefinition> subAlarmMetricDefinitions) {
+  public AlarmDeletedEvent(String tenantId, String alarmId, String alarmDefinitionId) {
     this.tenantId = tenantId;
     this.alarmId = alarmId;
-    this.subAlarmMetricDefinitions = subAlarmMetricDefinitions;
+    this.alarmDefinitionId = alarmDefinitionId;
   }
 
   @Override
@@ -49,18 +42,18 @@ public class AlarmDeletedEvent implements Serializable {
       return true;
     if (obj == null)
       return false;
-    if (getClass() != obj.getClass())
+    if (!(obj instanceof AlarmDeletedEvent))
       return false;
     AlarmDeletedEvent other = (AlarmDeletedEvent) obj;
+    if (alarmDefinitionId == null) {
+      if (other.alarmDefinitionId != null)
+        return false;
+    } else if (!alarmDefinitionId.equals(other.alarmDefinitionId))
+      return false;
     if (alarmId == null) {
       if (other.alarmId != null)
         return false;
     } else if (!alarmId.equals(other.alarmId))
-      return false;
-    if (subAlarmMetricDefinitions == null) {
-      if (other.subAlarmMetricDefinitions != null)
-        return false;
-    } else if (!subAlarmMetricDefinitions.equals(other.subAlarmMetricDefinitions))
       return false;
     if (tenantId == null) {
       if (other.tenantId != null)
@@ -74,16 +67,15 @@ public class AlarmDeletedEvent implements Serializable {
   public int hashCode() {
     final int prime = 31;
     int result = 1;
+    result = prime * result + ((alarmDefinitionId == null) ? 0 : alarmDefinitionId.hashCode());
     result = prime * result + ((alarmId == null) ? 0 : alarmId.hashCode());
-    result = prime * result
-        + ((subAlarmMetricDefinitions == null) ? 0 : subAlarmMetricDefinitions.hashCode());
     result = prime * result + ((tenantId == null) ? 0 : tenantId.hashCode());
     return result;
   }
 
   @Override
   public String toString() {
-    return String.format("AlarmDeletedEvent [tenantId=%s, alarmId=%s, subAlarmIds=%s]", tenantId,
-        alarmId, subAlarmMetricDefinitions);
+    return "AlarmDeletedEvent [tenantId=" + tenantId + ", alarmId=" + alarmId
+        + ", alarmDefinitionId=" + alarmDefinitionId + "]";
   }
 }
