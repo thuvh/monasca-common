18b7817b61d33143b37d823453a9b8bce026f2bd
Witold Bedyk <witold.bedyk@est.fujitsu.com>
2015-05-28 15:02:47 +0200

    corrected comments
    Change-Id: I44427bddc408031f393c460170567448575d8125

--- java/monasca-common-model/src/main/java/monasca/common/model/log/Log.java
+++ java/monasca-common-model/src/main/java/monasca/common/model/log/Log.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+ * Copyright (c) 2015 Fujitsu Limited
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -15,11 +15,7 @@ package monasca.common.model.log;
 
 import java.io.Serializable;
 
-import com.google.common.base.Preconditions;
-
 import javax.annotation.Nullable;
-import javax.validation.constraints.NotNull;
-
 import java.util.Map;
 
 /**
