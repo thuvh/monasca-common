96c526ab5138c64a455c5fe107e64ef1233c67cb
oiskam1 <oiskam1@yandex.ru>
2015-12-02 10:25:29 +0100

    Database agnostic implementation.
    Change-Id: I7a590550702ddb624046c7c8b40022bab0964925

---
+++ java/monasca-common-jooq/src/main/java/monasca/common/jooq/configuration/JooqDbConfiguration.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2015 FUJITSU LIMITED
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
+
+package monasca.common.jooq.configuration;
+
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class JooqDbConfiguration {
+
+  @NotNull
+  @JsonProperty
+  boolean supportEnabled;
+  @NotEmpty
+  @JsonProperty
+  String providerClass;
+  @NotEmpty
+  @JsonProperty
+  String dataSourceClassName;
+  @NotEmpty
+  @JsonProperty
+  String user;
+  @NotEmpty
+  @JsonProperty
+  String password;
+
+  @JsonProperty
+  String initialConnections;
+  @JsonProperty
+  String maxConnections;
+  @JsonProperty
+  String autoConfig;
+  @JsonProperty
+  String dataSourceUrl;
+  @JsonProperty
+  String serverName;
+  @JsonProperty
+  String portNumber;
+  @JsonProperty
+  String databaseName;
+
+  public String getDataSourceUrl() {
+    return this.dataSourceUrl;
+  }
+
+  public boolean getSupportEnabled() {
+    return supportEnabled;
+  }
+
+  public String getProviderClass() {
+    return providerClass;
+  }
+
+  public void setProviderClass(String providerClass) {
+    this.providerClass = providerClass;
+  }
+
+  public String getDataSourceClassName() {
+    return dataSourceClassName;
+  }
+
+  public String getServerName() {
+    return serverName;
+  }
+
+  public String getPortNumber() {
+    return portNumber;
+  }
+
+  public String getDatabaseName() {
+    return databaseName;
+  }
+
+  public String getUser() {
+    return user;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public String getInitialConnections() {
+    return initialConnections;
+  }
+
+  public String getMaxConnections() {
+    return maxConnections;
+  }
+}
