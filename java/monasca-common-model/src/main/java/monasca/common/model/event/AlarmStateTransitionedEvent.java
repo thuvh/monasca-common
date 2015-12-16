d6b68d5fd00bfe5eeb031ef1ec3ac4f2b105d6ac
Ryan Brandt <ryan.brandt@hp.com>
2015-12-16 13:27:23 -0700

    Pass link and lifecycle state in state transitions
    Requires changes to monasca-api, monasca thresh to use
    
    Change-Id: I7b03be0c28638ae5fd7dae7c6566036acb94797f

--- java/monasca-common-model/src/main/java/monasca/common/model/event/AlarmStateTransitionedEvent.java
+++ java/monasca-common-model/src/main/java/monasca/common/model/event/AlarmStateTransitionedEvent.java
@@ -36,6 +36,8 @@ public class AlarmStateTransitionedEvent {
   public boolean actionsEnabled;
   public String stateChangeReason;
   public String severity;
+  public String link;
+  public String lifecycleState;
   public List<AlarmTransitionSubAlarm> subAlarms;
   /** POSIX timestamp */
   public long timestamp;
@@ -44,8 +46,9 @@ public class AlarmStateTransitionedEvent {
 
   public AlarmStateTransitionedEvent(String tenantId, String alarmId, String alarmDefinitionId,
       List<MetricDefinition> metrics, String alarmName, String alarmDescription,
-      AlarmState oldState, AlarmState newState, String severity, boolean actionsEnabled,
-      String stateChangeReason, List<AlarmTransitionSubAlarm> subAlarms, long timestamp) {
+      AlarmState oldState, AlarmState newState, String severity, String link, String lifecycleState,
+      boolean actionsEnabled, String stateChangeReason, List<AlarmTransitionSubAlarm> subAlarms,
+      long timestamp) {
     this.tenantId = tenantId;
     this.alarmId = alarmId;
     this.alarmDefinitionId = alarmDefinitionId;
@@ -55,6 +58,8 @@ public class AlarmStateTransitionedEvent {
     this.oldState = oldState;
     this.newState = newState;
     this.severity = severity;
+    this.link = link;
+    this.lifecycleState = lifecycleState;
     this.actionsEnabled = actionsEnabled;
     this.stateChangeReason = stateChangeReason;
     this.subAlarms = subAlarms;
@@ -96,6 +101,16 @@ public class AlarmStateTransitionedEvent {
       if (other.severity != null)
         return false;
     } else if (!severity.equals(other.severity))
+      return false;
+    if (link == null) {
+      if (other.link != null)
+        return false;
+    } else if (!link.equals(other.link))
+      return false;
+    if (lifecycleState == null) {
+      if (other.lifecycleState != null)
+        return false;
+    } else if (!lifecycleState.equals(other.lifecycleState))
       return false;
     if (subAlarms == null) {
       if (other.subAlarms != null)
@@ -151,7 +166,8 @@ public class AlarmStateTransitionedEvent {
     return "AlarmStateTransitionedEvent [tenantId=" + tenantId + ", alarmId=" + alarmId
         + ", alarmDefinitionId=" + alarmDefinitionId + ", metrics=" + metrics + ", alarmName="
         + alarmName + ", alarmDescription=" + alarmDescription + ", oldState=" + oldState
-        + ", newState=" + newState + ", severity=" + severity + ", actionsEnabled=" + actionsEnabled + ", stateChangeReason="
+        + ", newState=" + newState + ", severity=" + severity + ", link=" + link
+        + ", lifecycleState=" + lifecycleState + ", actionsEnabled=" + actionsEnabled + ", stateChangeReason="
         + stateChangeReason + ", subAlarms=" + subAlarms +  ", timestamp=" + timestamp + "]";
   }
 }
