ebb11fa867cdd1ae120be5fc503cf53a38477063
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2016-03-15 08:47:39 +0100

    (Non)deterministic support for alarms
    Alarms can be created with deterministic keyword.
    'deterministic' can be passed as part of alarm expression using
    one of following forms:
    
     * keyword deterministic
     * deterministic={true,yes,1}
     * deterministic={false,no,0}
    
    If 'deterministic' is not found in expression it is assumed
    to be 'true'.
    
    Implements: blueprint alarmsonlogs
    Change-Id: Ia42f9a1be37c31416bdac341b092fe527f860c16

--- java/monasca-common-model/src/main/java/monasca/common/model/alarm/AlarmExpression.java
+++ java/monasca-common-model/src/main/java/monasca/common/model/alarm/AlarmExpression.java
@@ -151,12 +151,35 @@ public class AlarmExpression {
   public List<AlarmSubExpression> getSubExpressions() {
     if (subExpressions != null)
       return subExpressions;
-    List<AlarmSubExpression> subExpressions = new ArrayList<AlarmSubExpression>();
+    List<AlarmSubExpression> subExpressions = new ArrayList<>(elements.size());
     for (Object element : elements)
       if (element instanceof AlarmSubExpression)
         subExpressions.add((AlarmSubExpression) element);
     this.subExpressions = subExpressions;
     return subExpressions;
+  }
+
+  /**
+   * Returns if expression is deterministic or non-deterministic.
+   *
+   * All {@link AlarmSubExpression} must be non-deterministic in order for entire expression
+   * to be such. Otherwise expression is deterministic.
+   *
+   * @return true/false
+   *
+   * @see #getSubExpressions()
+   */
+  public boolean isDeterministic() {
+    final List<AlarmSubExpression> subExpressions = this.getSubExpressions();
+    if (subExpressions == null || subExpressions.isEmpty()) {
+      return AlarmSubExpression.DEFAULT_DETERMINISTIC;
+    }
+    for (final AlarmSubExpression alarmSubExpression : subExpressions) {
+      if (alarmSubExpression.isDeterministic()) {
+        return true;
+      }
+    }
+    return false;
   }
 
   @Override
