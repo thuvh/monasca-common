778fdfefc2b1ea062f9899dc3bb1c3041c444260
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2016-03-15 08:47:39 +0100

    Periodicity for metrics
    Commit provides periodicity for metrics.
    Period is done per metric so each one
    of them can be processed invididually.
    Along with field, metric model is self
    aware of its own periodicity. Those
    capabilities can be used by external
    processing tools to speed up evaluation.
    
    Implements: blueprint alarmsonlogs
    Change-Id: Ia42f9a1be37c31416bdac341b092fe527f860c16

---
+++ java/monasca-common-model/src/main/java/monasca/common/model/metric/MetricPeriod.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2016 FUJITSU LIMITED
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package monasca.common.model.metric;
+
+/**
+ * Utility class to determine periodicity or infrequency
+ * of given metric.
+ */
+public class MetricPeriod {
+  /**
+   * Defines threshold that if compared with
+   * period value answers is metric is periodic or sparse
+   */
+  private static final long THRESHOLD = 0L;
+
+  private MetricPeriod(){}
+
+  public static boolean isSparse(final Metric metric){
+    return metric != null && isSparse(metric.getPeriod());
+  }
+
+  public static boolean isSparse(final long period){
+    return period < THRESHOLD;
+  }
+
+  public static boolean isPeriodic(final Metric metric){
+    return metric != null && isPeriodic(metric.getPeriod());
+  }
+
+  public static boolean isPeriodic(final long period){
+    return period > THRESHOLD;
+  }
+
+}
