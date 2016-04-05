9947b4f0e881e7d2265f27e11fb1fe9af2fd9383
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2016-04-05 07:10:34 +0200

    Don't use deprecated partitioner
    LegacyPartitioner (alias for HashedPartitioner) is
    depracated. This commit switches by default to Murmur2
    partitioner (mimics behaviour from Java implementation).
    
    Additionally:
    * addded possibility to use RoundRobin
    partitionar.
    * updated docstring
    
    Change-Id: I47796b863ef7d3f799c347c26e9a11eb4eeade4c

--- monasca_common/kafka/producer.py
+++ monasca_common/kafka/producer.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2015 Hewlett-Packard Development Company, L.P.
+# Copyright 2016 FUJITSU LIMITED
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,6 +15,7 @@
 # limitations under the License.
 
 import kafka.client
+import kafka.partitioner
 import kafka.producer
 import logging
 import time
@@ -25,19 +27,39 @@ class KafkaProducer(object):
     """Adds messages to a kafka topic
     """
 
-    def __init__(self, url):
-        """Init
-             url - kafka connection details
+    def __init__(self, url, round_robin=False):
+        """Creates new kafka producer
+
+        Note:
+            By default producer is created with
+            :py:class:'kafka.partitioner.Murmur2Partitioner'.
+            If round robin partitioner (i.e. fair) should be used
+            set 'round_robin'=True
+
+        :arg str|tuple|list url: host(s) to connect to
+        :arg bool round_robin: should fair round robin partitioner be used
+
         """
+
+        if round_robin:
+            partitioner = kafka.partitioner.RoundRobinPartitioner
+        else:
+            partitioner = kafka.partitioner.Murmur2Partitioner
+
         self._kafka = kafka.client.KafkaClient(url)
         self._producer = kafka.producer.KeyedProducer(
             self._kafka,
             async=False,
             req_acks=kafka.producer.KeyedProducer.ACK_AFTER_LOCAL_WRITE,
-            ack_timeout=2000)
+            ack_timeout=2000,
+            partitioner=partitioner)
 
     def publish(self, topic, messages, key=None):
         """Takes messages and puts them on the supplied kafka topic
+
+        :param str topic: target topic
+        :param list|str messages: single message or list of messages
+        :param str key: key for set of messages
         """
 
         if not isinstance(messages, list):
