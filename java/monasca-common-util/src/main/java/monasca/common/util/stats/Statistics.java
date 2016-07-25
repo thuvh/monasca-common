5c5defe50de1edcce1a89355778e82f91d01e2cf
Craig Bryant <craig.bryant@hp.com>
2016-07-24 20:58:34 -0600

    Add Last function
    Add the LAST function to the Java parser and its associated classes.
    
    Update SubAlarmDb to have a state field.
    
    The changes to Java Monasca API and Monasca Thresh for this last-value
    will require these changes, first
    
    Also, remove all trailing spaces from AlarmExpression.g4
    
    Change-Id: Ie3192370057676e89bfebbc5ce4243b1a3ecf0ea
    Implements: Blueprint last-value

--- java/monasca-common-util/src/main/java/monasca/common/util/stats/Statistics.java
+++ java/monasca-common-util/src/main/java/monasca/common/util/stats/Statistics.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+ * (C) Copyright 2014, 2016 Hewlett Packard Enterprise Development LP
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -102,6 +102,14 @@ public final class Statistics {
     public void addValue(double value) {
       initialized = true;
       this.value += value;
+    }
+  }
+
+  public static class Last extends AbstractStatistic {
+    @Override
+    public void addValue(double value) {
+      initialized = true;
+      this.value = value;
     }
   }
 
