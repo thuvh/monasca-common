df1d97b19d24463ea8207fcbf3011acf2a3bf51c
Joachim Barheine <joachim.barheine@sap.com>
2017-02-24 09:43:04 +0100

    Add hibernate query for deletion of alarm-definitions
    This query can be used by the thresholder to remove the alarm-definitions
    from the config-DB after they have been removed from the Storm
    topology.
    
    Closes-Bug: #1661322
    
    Change-Id: I14d07e02371415d7afa8c6e7bc1ca358f6697d71

--- java/monasca-common-hibernate/src/main/java/monasca/common/hibernate/db/AlarmDefinitionDb.java
+++ java/monasca-common-hibernate/src/main/java/monasca/common/hibernate/db/AlarmDefinitionDb.java
@@ -49,6 +49,10 @@ import monasca.common.model.alarm.AlarmSeverity;
 })
 @NamedQueries({
     @NamedQuery(
+        name = AlarmDefinitionDb.Queries.DELETE_BY_ID,
+        query = "delete from AlarmDefinitionDb a where a.id = :id"
+    ),
+    @NamedQuery(
         name = AlarmDefinitionDb.Queries.FIND_BY_TENANT_AND_ID_NOT_DELETED,
         query = "from AlarmDefinitionDb ad " +
             "where ad.tenantId = :tenant_id " +
@@ -259,6 +263,7 @@ public class AlarmDefinitionDb
   }
 
   public interface Queries {
+    String DELETE_BY_ID = "AlarmDefinition.deleteById";
     String FIND_BY_TENANT_AND_ID_NOT_DELETED = "AlarmDefinition.byTenantAndIdNotDeleted";
     String FIND_BY_TENANT_ID_AND_ID = "AlarmDefinition.byTenantIdAndId";
   }
