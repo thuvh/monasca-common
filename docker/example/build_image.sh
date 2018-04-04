da3ef8c352a2ec600e39da89565625f7f3ea8328
Dobroslaw Zybort <dobroslaw.zybort@ts.fujitsu.com>
2018-04-04 14:09:15 +0200

    Add base Dockerfile and supporting scripts
    Story: 2001694
    Task: 12491
    
    Change-Id: I81e0d0ecbb431ed7e26fcbcb4d347ac164c66736

---
+++ docker/example/build_image.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# TODO(Dobroslaw): move this script to monasca-common/docker folder
+# and leave here small script to download it and execute using env variables
+# to minimize code duplication.
+
+
+set -x
+# This script is used for building Docker image with proper labels.
+# To build specific version run this script in the following way:
+# $ ./build_image.sh stable/queens
+
+: "${VERSION:=$1}"
+[ -z "$VERSION" ] && VERSION=$(grep VERSION Dockerfile | cut -f2 -d"=")
+
+# Image name can be overidden with env var.
+: "${DOCKER_IMAGE:=monasca/api}"
+
+[ -z "$REPO_PATH" ] && REPO_PATH=$(grep REPO_PATH Dockerfile | cut -f2 -d"=")
+GITHUB_REPO=$(echo "$REPO_PATH" | sed 's/git.openstack.org/github.com/' | \
+              sed 's/ssh:/https:/')
+
+# Clone project to temporary directory for getting proper commit number from
+# branches and tags. We need this for setting proper image labels.
+# Docker does not allow to get any data from inside of system when building
+# image.
+TMP_DIR=$(mktemp -d)
+git clone "$REPO_PATH" "$TMP_DIR"
+cd "$TMP_DIR" || exit 1
+GIT_COMMIT=$(git rev-list "$VERSION" --max-count=1)
+cd - || exit 1
+rm -rf "$TMP_DIR"
+
+CREATION_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
+# Build-time metadata as defined at
+# https://github.com/opencontainers/image-spec/blob/master/annotations.md
+
+docker build --no-cache \
+    --build-arg VERSION="$VERSION" \
+    --label org.opencontainers.image.created="$CREATION_TIME" \
+    --label org.opencontainers.image.url="$GITHUB_REPO" \
+    --label org.opencontainers.image.source="$REPO_PATH" \
+    --label org.opencontainers.image.version="$VERSION" \
+    --label org.opencontainers.image.revision="$GIT_COMMIT" \
+    --label org.opencontainers.image.title="$DOCKER_IMAGE" \
+    --label org.opencontainers.image.licenses="Apache-2.0" \
+    --label org.openstack.constraints_file="" \
+    --tag "$DOCKER_IMAGE":"$VERSION" \
+    --tag "$DOCKER_IMAGE":"$VERSION"-"$CREATION_TIME" .
