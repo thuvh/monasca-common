eb7b1041864f75751ed93dbcc9cedbc76dd22a5c
Dobroslaw Zybort <dobroslaw.zybort@ts.fujitsu.com>
2018-04-04 14:09:15 +0200

    Add base Dockerfile and supporting scripts
    Story: 2001694
    Task: 12491
    
    Change-Id: I81e0d0ecbb431ed7e26fcbcb4d347ac164c66736

---
+++ docker/example/start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Test services we need before starting our service
+python3 /kafka_wait_for_topics.py || exit 1
+
+# Template all config files before start
+
+# Start our service
+gunicorn -n monasca-api
