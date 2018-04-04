8a2203d6c46133d10a244da0e506a51120627dcf
Dobroslaw Zybort <dobroslaw.zybort@ts.fujitsu.com>
2018-04-04 14:09:15 +0200

    Add base Dockerfile and supporting scripts
    Story: 2001694
    Task: 12491
    
    Change-Id: I81e0d0ecbb431ed7e26fcbcb4d347ac164c66736

---
+++ docker/example/start.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Starting script.
+# All checks you need to do before service could be safely started should
+# be added in this file.
+
+# Test services we need before starting our service.
+echo "Start script: waiting for needed services"
+python3 /kafka_wait_for_topics.py || exit 1
+python3 /mysql_check.py || exit 1
+
+# Put all static (not configurable by user) envirmnet variables here.
+# export GUNICORN_BACKLOG=1000
+
+# Template all config files before start, it will use env variables.
+# Read usage examples: https://pypi.org/project/Templer/
+echo "Start script: creating config files from templates"
+templer /*.j2 /
+
+# Start our service.
+# gunicorn --args
+echo "Start script: starting container"
+
+# Allow server to stay alive in case of failure for 2 hours for debugging.
+RESULT=$?
+if [ $RESULT != 0 ] && [ "$STAY_ALIVE_ON_FAILURE" = "true" ]; then
+  echo "Service died, waiting 120 min before exiting"
+  sleep 7200
+fi
+exit $RESULT
