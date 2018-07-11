0d14966db35ff6515091a9408e1759ab671b6a65
Dobroslaw Zybort <dobroslaw.zybort@ts.fujitsu.com>
2018-07-11 14:10:34 +0200

    [WIP] Build Docker image in zuul
    Change-Id: I554e1efdfb7b996d805748a7930a76e2124e060b

---
+++ playbooks/build_image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -o xtrace
+set -o errexit
+
+REAL_PATH=$(python -c "import os,sys;print(os.path.realpath('$0'))")
+cd "$(dirname "$REAL_PATH")/.."
+
+docker build --no-cache -t monasca-base:1.0.0 .
+
+docker images
