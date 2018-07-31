5de933237a41d10ffbcd82fa53bdf7eb8dc5f182
Witold Bedyk <witold.bedyk@est.fujitsu.com>
2018-07-31 11:07:06 +0200

    Remove Docker tag based on timestamp
    As agreed in spec [1] we don't want to create and publish tags based on
    timestamp.
    
    Story: 2001694
    Task: 23322
    
    [1] http://specs.openstack.org/openstack/monasca-specs/specs/rocky/approved/monasca-services-in-docker.html#work-items
    
    Change-Id: Id72eb0ac7be7077bfaa8b8684a45494c09d92034

--- docker/example/build_image.sh
+++ docker/example/build_image.sh
@@ -84,8 +84,6 @@ rm -rf "$TMP_DIR"
 # we will be using with app.
 
 CREATION_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-# Docker tags don't like colons so use shorter version of ISO 8601 for them.
-CREATION_TIME_SHORT=$(date -d "$CREATION_TIME" -u +"%Y%m%dT%H%M%SZ")
 
 docker build --no-cache \
     --build-arg CREATION_TIME="$CREATION_TIME" \
@@ -94,5 +92,4 @@ docker build --no-cache \
     --build-arg REPO_VERSION="$REPO_VERSION" \
     --build-arg GIT_COMMIT="$GIT_COMMIT" \
     --build-arg CONSTRAINTS_BRANCH="$CONSTRAINTS_BRANCH_CLEAN" \
-    --tag "$DOCKER_IMAGE":"$REPO_VERSION_CLEAN" \
-    --tag "$DOCKER_IMAGE":"$REPO_VERSION_CLEAN"-"$CREATION_TIME_SHORT" .
+    --tag "$DOCKER_IMAGE":"$REPO_VERSION_CLEAN" .
