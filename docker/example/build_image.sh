673c44c2bc5c451daee111597eb327b5d224a490
zhulingjie <easyzlj@gmail.com>
2019-04-23 16:41:16 +0800

    Replace git.openstack.org URLs with opendev.org URLs
    Change-Id: I52c17b2fcf73decfd1b0afd60ad82a030279570d

--- docker/example/build_image.sh
+++ docker/example/build_image.sh
@@ -60,7 +60,7 @@ GITHUB_REPO=$(echo "$APP_REPO" | sed 's/git.openstack.org/github.com/' | \
 
 if [ -z "$CONSTRAINTS_FILE" ]; then
     CONSTRAINTS_FILE=$(\grep CONSTRAINTS_FILE Dockerfile | cut -f2 -d"=") || true
-    : "${CONSTRAINTS_FILE:=http://git.openstack.org/cgit/openstack/requirements/plain/upper-constraints.txt}"
+    : "${CONSTRAINTS_FILE:=http://opendev.org/openstack/requirements/plain/upper-constraints.txt}"
 fi
 
 : "${CONSTRAINTS_BRANCH:=$2}"
